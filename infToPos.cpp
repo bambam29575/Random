73464311ddcd5d477de508f50437ebe0d17e2650
bambam29575 <123730907+bambam29575@users.noreply.github.com>
2024-01-25 23:40:04 +0530

    Update infToPos.cpp

--- infToPos.cpp
+++ infToPos.cpp
@@ -6,57 +6,65 @@ string stack, infix, postfix;
 int i = 0, j = 0, k = 0, m = 0;
 char ch;
 
-void pop(char ch){
-    
+void pop(char ch) {}
+
+int precedence(char ch) {
+  if (ch == '+' || ch == '-') {
+    return 1;
+  } else if (ch == '*' || ch == '/') {
+    return 2;
+  } else
+    return 3;
 }
 
-int precedence(char ch){
-    if(ch == '+' || ch == '-' ){
-        return 1;
+int isOperator(char ch) {
+    cout << ch<< "2" <<endl;
+  if (ch == '/' || ch == '*' || ch == '-' || ch == '+') {
+    return 1;
+  } else
+    return 0;
+}
+
+void infToPos(string infix) {
+  while (infix[m] != '\0') {
+       cout << m << "1" << endl;
+    if (isOperator(infix[m]) == 0) {
+    cout << isOperator(infix[m]) << "3" << endl;
+      postfix[j] = infix[m];
+    //   cout << postfix[j];
+      j++;
+    //   cout << postfix[j];
+    cout << infix[m] << "4" <<endl;
+    cout << m << "5" << endl;
+     cout << "c6" << endl;
+     cout << endl;
     }
-    else if(ch == '*' || ch == '/' ){
-        return 2;
-    }else return 3;
-    
-}
-
-int isOperator(char ch){
-    if(ch == '/' || ch == '*' || ch == '-' || ch == '+'){
-        return 1;
-    }else return 0;
-}
-
-string infToPos(string infix){
-    while(infix[m] != '\0'){
-    if(isOperator(infix[i]) != 1){
-       postfix[j] = ch;
-          j++;
-    }
-    if (isOperator(infix[i]) == 1){
-        if(k > 0){
+    if (isOperator(infix[i]) == 1) {
+        cout << "c" << endl;
+      if (k > 0) {
         // stack[k] = ch;
         // k++;
-        if (precedence(stack[k]) <= precedence(stack[k-1])){
-            int temp = k;
-            do{
+        if (precedence(stack[k]) <= precedence(stack[k - 1])) {
+          int temp;
+          temp = k;
+          do {
             pop(stack[k]);
             k--;
-            }while(precedence(stack[k]) == 1);
+          } while (precedence(stack[k]) == 1);
         }
-        }
+      }
+      stack[k] = infix[m];
+      cout<< stack[k];
     }
     m++;
-    }
+  }
 }
 
-
-
 int main() {
-    cout << "Enter infix  expression" << endl;
-    cin >> infix;
-    postfix = infToPos(infix);
-    cout << postfix << endl;
-    
+  cout << "Enter infix  expression" << endl;
+  cin >> infix;
+  infToPos(infix);
+  // cout << postfix << endl;
 
-    return 0;
+  return 0;
 }
